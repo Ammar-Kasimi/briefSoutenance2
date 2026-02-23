3db7709542255487ea31da14740432e0d33d409e
Ammar-Kasimi <kasimiammar12@gmail.com>
2026-02-23 15:46:52 +0000

    added both the diagrams and made most of the base  structure (migrations ,empty models and controllers)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username')->unique();
+            $table->string('email', 190)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 1000);
+            $table->boolean("isActive")->default(true);
+            $table->boolean("isOwner")->default(false);
+            $table->foreignId('collocation_id')->constrained();
+            $table->foreignId('role_id')->constrained();
             $table->rememberToken();
             $table->timestamps();
         });
